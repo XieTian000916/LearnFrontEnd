3327a4109c4b18f853a1bc62f7a6d05ef53b1a8f
XieTian000916 <xietian1149@gmail.com>
2022-10-01 22:07:19 +0800

    01/10/2022 Update

---
+++ Learn_Vue/15_case_todolist/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div class="todo-container">
+      <div class="todo-warp">
+        <todo-header :addTodo="addTodo"/>
+        <todo-main :todos="todos"/>
+        <todo-footer/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoMain from "./components/TodoMain.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+import {nanoid} from 'nanoid'
+
+export default {
+  name: "App",
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+  },
+  data() {
+    return {
+      todos:[{id:nanoid(),title:'欢迎使用TodoList',done:true}]
+    }
+  },
+  methods: {
+    addTodo(todoObj){
+      this.todos.unshift(todoObj);
+    }
+  },
+};
+</script>
+
+<style>
+/* base */
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-warp {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ Learn_Vue/15_case_todolist/src/components/TodoMain.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="todo-main">
+    <todo-item v-for="todoObj in todos" v-bind:key="todoObj.id" :todo="todoObj"/>
+  </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  name: "TodoMain",
+  components:{
+    TodoItem
+  },
+  props:['todos']
+};
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>

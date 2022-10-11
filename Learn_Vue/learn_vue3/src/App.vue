4f8292248d1ae9eb9da2276f528e28537ca10415
XieTian000916 <xietian1149@gmail.com>
2022-10-11 11:03:59 +0800

    11/20/2022 1st update try vue3

---
+++ Learn_Vue/learn_vue3/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ msg.split(" ").map((v) => `#${v}`) }}</div>
+  <!-- 模板内可跟api -->
+</template>
+
+<script setup lang="ts">
+const msg: string = "try Vue3";
+</script>
+
+<style></style>
